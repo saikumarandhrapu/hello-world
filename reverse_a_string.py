c1a395e35af3d4a8730b3d81dd81ed50e7169ca5
saikumarandhrapu <saikumar.andhrapu93@gmail.com>
2020-05-19 19:05:20 +0530

    changed again

---
+++ reverse_a_string.py
@@ -0,0 +1,12 @@
+x=input("enter the string")
+l=x.strip()
+l1=[]
+i=len(l)-1
+while i>=0:
+    l1.append(l[i])
+    i-=1
+output="".join(l1)
+print(output)
+print("changes made")
+print("one more change")
+print("hello world from PC")
